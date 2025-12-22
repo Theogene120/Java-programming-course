f3c48bc7c3e11efc9fb0cb8f4d7eceb2355b5ba6
Theogene <igiranezatheogene4@gmail.com>
2025-12-22 22:59:52 +0200

    add sum

---
+++ cat1correction/Sum.java
@@ -0,0 +1,19 @@
+package cat1correction;
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String [] args){
+        Scanner th = new Scanner(System.in);
+        int num1, num2;
+        int sum;
+
+        System.out.print("Enter first number:");
+        num1 = th.nextInt();
+        System.out.print("Enter second number:");
+        num2 = th.nextInt(); 
+
+        sum = num1 + num2;
+
+        System.out.println("Sum of Entered number is: " + sum);
+    }
+}
