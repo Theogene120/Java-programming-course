535b90cbd5a7aae3093b2f1cbd8360a863ce6c9e
Theogene <igiranezatheogene4@gmail.com>
2025-12-23 16:22:40 +0200

    Addition code

--- Array.java
+++ Array.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Array {
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5};
+        int arr[] = {3,5,1,4,2};
         int sum = 0;
         for(int i = 0; i < arr.length; i++){
             sum += arr[i];
@@ -9,5 +11,9 @@ public class Array {
         System.out.println(sum);
 
         System.out.println("Coded by Theos");
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr)); 
     }
 }
