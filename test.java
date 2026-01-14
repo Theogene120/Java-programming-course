5f8f486d75cdf59026c3a117b799f415bb71912d
Theogene <igiranezatheogene4@gmail.com>
2026-01-14 11:57:03 +0200

    output from exam

--- test.java
+++ test.java
@@ -1,1 +1,13 @@
-
+public class test {
+    public static void main (String args[]){
+        try{
+            System.out.print("Hello");
+            int num = 1/0;
+            System.out.print("World");
+        }catch (ArithmeticException e){
+            System.out.print(" java programmer");
+        }finally{
+            System.out.print("!!");
+        }
+    }
+}
