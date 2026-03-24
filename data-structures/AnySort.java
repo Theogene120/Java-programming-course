4639326858678748098c1479f82f6250574ea441
Theogene <igiranezatheogene4@gmail.com>
2026-03-24 11:53:07 +0200

    All sorts method added

--- data-structures/AnySort.java
+++ data-structures/AnySort.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class AnySort {
 
@@ -70,9 +72,80 @@ public class AnySort {
 
     }
 
+    public static void insertionSort(int[] arr) { // insertion sort function
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // the element to be inserted
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1] that are greater than key
+            // one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        // Display the sorted list
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void shellSort(int[] arr) {   // Shell sort function
+        int n = arr.length;
+
+        // Start with a big gap, then reduce the gap
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            // Do a gapped insertion sort for this gap size
+            for (int i = gap; i < n; i++) {
+                int temp = arr[i];
+                int j = i;
+
+                // Shift earlier gap-sorted elements up until the correct location is found
+                while (j >= gap && arr[j - gap] > temp) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+
+                // Put temp (the original arr[i]) in its correct location
+                arr[j] = temp;
+            }
+        }
+
+        // Display the sorted list
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
     public static void main(String[] args) {
         int[] generatedList = generateRandomNumbers();
-        
-        selectionSort(generatedList);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sorting algorithm (bubble / selection / insertion / shell):");
+        String choice = sc.nextLine().toLowerCase();
+
+        switch (choice) {
+            case "bubble":
+                bubbleSort(generatedList);
+                break;
+            case "selection":
+                selectionSort(generatedList);
+                break;
+            case "insertion":
+                insertionSort(generatedList);
+                break;
+            case "shell":
+                shellSort(generatedList);
+                break;
+            default:
+                System.out.println("Invalid choice! Array will not be sorted.");
+                return;
+        }
+
     }
 }
