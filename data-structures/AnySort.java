40f81bf6e72879dfd98cd891aec97401938fe8c4
Theogene <igiranezatheogene4@gmail.com>
2026-04-26 18:37:04 +0200

    changes

--- data-structures/AnySort.java
+++ data-structures/AnySort.java
@@ -6,9 +6,9 @@ public class AnySort {
 
     public static int[] generateRandomNumbers() {
         Random rand = new Random();
-        int[] numbers = new int[5];
+        int[] numbers = new int[10];
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             numbers[i] = rand.nextInt(1000) + 1; // range 1–1000
         }
 
