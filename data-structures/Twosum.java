6d1cc74b116c844d7e9a590136480d446fba965f
Theogene <igiranezatheogene4@gmail.com>
2026-05-01 19:26:52 +0200

    some improvement

--- data-structures/Twosum.java
+++ data-structures/Twosum.java
@@ -2,7 +2,9 @@ public class Twosum {
     
     public static int sumoftwo(int[] a){
         int n = a.length;
-        
-        return n;
+        int left = 0;
+        int right = n - 1;
+
+        return left + right;
     }
 }
